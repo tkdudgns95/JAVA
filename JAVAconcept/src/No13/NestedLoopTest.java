b4467fd3d9a4a0b27a980659c1cade1b18fbf964
tkdudgns95 <tkdudgns95@naver.com>
2022-12-04 22:37:20 +0900

    중첩 반복문

---
+++ JAVAconcept/src/No13/NestedLoopTest.java
@@ -0,0 +1,39 @@
+package No13;
+
+public class NestedLoopTest {
+
+	public static void main(String[] args) {
+		/* 반복문 내부에 또 다른 반복문이 있음
+		 * 여러 겹으로 반복문을 겹쳐서 구현가능(단 수행시간에 문제가 발생할 수 있음.)
+		 * 외부 반복문과 내부 반복문 간의 변수 값 변화에 유의하며 구현하여야 함.
+		 */ 
+		int dan = 2;
+		int count = 1;
+		
+	/*	for(dan = 2; dan <= 9; dan ++ ) {
+			
+			for(count = 1; count < 9; count++) {
+				
+				System.out.println( dan + "X" + count + "=" + dan*count);
+			}
+			
+			System.out.println();
+		}*/
+	
+		dan = 2;
+		count = 1;
+		 while(dan <= 9) {
+			
+			 count = 1;
+			while(count <= 9) {
+				System.out.println( dan + "X" + count + "=" + dan*count);
+				count++;
+				
+			}
+			dan++;
+			System.out.println();
+			
+		}
+	}
+
+}
