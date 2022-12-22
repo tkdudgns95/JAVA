482c8e935c8c0f52a7d35ac2ac8340fbcf1233c7
tkdudgns95 <tkdudgns95@naver.com>
2022-12-22 09:36:08 +0900

    싱글톤 패턴

---
+++ JAVAconcept/src/No25/Company.java
@@ -0,0 +1,28 @@
+package No25;
+
+public class Company {
+	/* static 응용 - 싱글톤 패턴
+	 * 싱글톤 패턴이란
+	 * 프로그램에서 인스턴스가 단 한 개만 생성되어야 하는 경우 사용하는 디자인 패턴
+	 * static 변수, 메서드를 활용하여 구현 할 수 있음
+	 * 
+	 * 
+	 */
+	
+	private static Company instance = new Company();
+	
+	private Company() {
+		
+	}
+	
+	public static Company getInstance() {
+		
+		if (instance == null) {
+			
+			instance = new Company();
+			
+		}
+		
+		return instance;
+	}
+}
